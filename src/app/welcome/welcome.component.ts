2aa4c26d0b4c9d7a1ca910583fabed41fd1bdfd7
phuongntm8449 <phuongntm8449@co-well.com.vn>
2020-09-03 17:17:53 +0700

    create new project

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+// import {AppComponent} from '../app.component';
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+  message = 'Some Welcome Message';
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.message);
+  }
+
+}
